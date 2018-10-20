c36cb3bd9421f4ec55c0b9f4216437e096060b7a
PencilBoxL <andrewliuzihua@gmail.com>
2018-10-20 14:42:44 +1100

    Test Image Upload

--- backend/app/Controllers/Http/PostController.js
+++ backend/app/Controllers/Http/PostController.js
@@ -667,14 +667,15 @@ class PostController {
   */
   async postIns({ request, response }) {
     try {
+      console.log('In')
       console.log(request.input('comment'))
       base64Image
         .img(request.input("postPic"), "../../../public/UserPost", "base64Test",function(err,filepath){
           console.log(err)
           console.log(filepath)
         })
-        base64ToImage(request.input('postPic'),'../../../public/UserPost',{'fileName':'base64Test2','type':'png'})
-        console.log('second')
+      base64ToImage(request.input('postPic'),'../../../public/UserPost',{'fileName':'base64Test2','type':'png'})
+      console.log('second')
         return 'success'
       //FIXME:Different file key for different content
       const postPic = request.file("postPic", {
